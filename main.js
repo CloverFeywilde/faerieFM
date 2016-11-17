07d89570cb22d6b42c23b22cf77d73e9634d0622
Bryan Laptop <bryanwillis7@gmail.com>
2016-11-17 09:51:29 -0500

    Added touch and mouse controls that allow the player to drag the main sprite around the screen. Going to clean up the code a bit, and restrict drag movement to the renderer only.

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ stage.addChild(container);
 
 //Aliases and Globals
 var Sprite = PIXI.Sprite;
+var state
 
 //Sprite creation & Setup function
 PIXI.loader
@@ -27,10 +28,62 @@ function setup(){
   var test = new Sprite(
     PIXI.loader.resources['images/tester.png'].texture  
   );
+test.interactive = true;
+test.buttonMode = true;
+test.anchor.set(0.5);
+test
+  .on('mousedown', onDragStart)
+  .on('touchstart', onDragStart)
+  .on('mouseup',onDragEnd)
+  .on('mouseupoutside', onDragEnd)
+  .on('touchend', onDragEnd)
+  .on('touchendoutside', onDragEnd)
+  .on('mousemove', onDragMove)
+  .on('touchmove', onDragMove);
+
+test.position.x = 100;
+test.position.y = 200;
 
 container.addChild(test);
+state = play;
+gameLoop();
+}
 
+//Game Loop
+function gameLoop(){
+requestAnimationFrame(gameLoop);
+state();
 renderer.render(stage);
 }
 
+//Game States
+function play(){
 
+}
+
+//Mouse Drag Functions
+function onDragStart(event)
+{
+    // store a reference to the data
+    // the reason for this is because of multitouch
+    // we want to track the movement of this particular touch
+    this.data = event.data;
+    this.dragging = true;
+}
+
+function onDragEnd()
+{
+    this.dragging = false;
+    // set the interaction data to null
+    this.data = null;
+}
+
+function onDragMove()
+{
+    if (this.dragging)
+    {
+        var newPosition = this.data.getLocalPosition(this.parent);
+        this.position.x = newPosition.x;
+        this.position.y = newPosition.y;
+    }
+}
